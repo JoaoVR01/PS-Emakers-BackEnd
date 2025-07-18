4078ebc25bcc394575fa3d51be3bf0f1b8433d15
joao <jrezendemarciano@gmail.com>
2025-07-18 15:35:21 -0300

    Criação do projeto com spring initialzr

---
+++ projeto-emakers/src/main/java/com/example/projeto_emakers/ProjetoEmakersApplication.java
@@ -0,0 +1,13 @@
+package com.example.projeto_emakers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoEmakersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoEmakersApplication.class, args);
+	}
+
+}
