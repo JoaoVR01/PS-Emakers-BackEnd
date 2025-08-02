8c40a08a7f6ff7cfef881629d9fdad84056d268f
joao <jrezendemarciano@gmail.com>
2025-08-02 19:34:05 -0300

    Correção de problemas

--- projetoBackEnd/src/main/java/emakersProjetoBackEnd/data/dto/request/LivroRequestDTO.java
+++ projetoBackEnd/src/main/java/emakersProjetoBackEnd/data/dto/request/LivroRequestDTO.java
@@ -3,6 +3,7 @@ package emakersProjetoBackEnd.data.dto.request;
 import java.time.LocalDate;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record LivroRequestDTO(
 
@@ -12,7 +13,7 @@ public record LivroRequestDTO(
     @NotBlank(message = "Autor is required")
     String autor,
 
-    @NotBlank(message = "release date is required")
+    @NotNull(message = "release date is required")
     LocalDate data_lancamento
 ) {
     
